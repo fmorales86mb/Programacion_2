9b10740b3866e0828c22646fe3123087c5da46b2
fmorales86mb <fmorales86mb@gmail.com>
2018-08-24 15:35:40 -0300

    Ejercicios de 1 a 6

---
+++ Guía de Ejercicios/ConceptosBasicos/Ejercicio_04/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio_04";            
+            int cantNrosPerfectos = 4;
+            
+            for (int i = 1; cantNrosPerfectos > 0; i++)
+            {
+                if (IsPerfecto(i))
+                {
+                    Console.WriteLine(i.ToString());
+                    cantNrosPerfectos--;
+                }
+            }
+            
+            Console.ReadKey();
+        }
+        
+        /// <summary>
+        /// Evalua si el nro es perfecto.
+        /// </summary>
+        /// <param name="nro">Entero positivo.</param>
+        /// <returns></returns>
+        private static bool IsPerfecto(int nro)
+        {
+            bool isPerfecto;
+            int acumulador = 0;           
+
+            for (int i = 1; i < nro; i++)
+            {
+                if (nro % i == 0) acumulador += i;                
+            }
+
+            isPerfecto = acumulador == nro ? true : false;
+
+            return isPerfecto;
+        }
+    }
+}
+
---
+++ Guía de Ejercicios/ConceptosBasicos/Ejercicio_05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibliotecaGuiaEjercicios;
+
+namespace Ejercicio_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio_05";
+            int nro;
+            string linea;
+            int acumuladoL = 0;
+            int toEval;
+
+            Console.Write("Ingrese Nro: ");
+            linea = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(linea) && int.TryParse(linea, out nro) )
+            {
+                for (int i = 1; i+1 < nro; i++)
+                {
+                    toEval = i + 1;
+                    acumuladoL += i;
+                    if (ConceptosBasicos.IsCentroNumerico(acumuladoL, toEval, nro)) Console.WriteLine(toEval.ToString());
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
