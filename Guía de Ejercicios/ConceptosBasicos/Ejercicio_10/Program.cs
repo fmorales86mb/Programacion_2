d471c745255bf09510a2e5a8218e523d3fc907d0
fmorales86mb <fmorales86mb@gmail.com>
2018-08-27 13:08:31 -0300

    Ejercicio 10 resuleto

---
+++ Guía de Ejercicios/ConceptosBasicos/Ejercicio_10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibliotecaGuiaEjercicios;
+
+namespace Ejercicio_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio_10";
+            int filas = 0;
+
+            if (ConceptosBasicos.PedirEntero("Número entero positivo: ", "Valor erroneo.", ref filas, 0))
+            {
+                Console.WriteLine();
+                for (int fila = 1; fila <= filas; fila++)
+                {
+                    for (int space = 0; space < filas - fila; space++)
+                    {
+                        Console.Write(' ');
+                    }
+                    for (int col = 1; col <= (2 * fila) - 1; col++)
+                    {                        
+                        Console.Write('*');
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
