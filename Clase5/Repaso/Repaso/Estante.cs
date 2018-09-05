facfaa68cd7bcfaf0fc02455c12b27f780c10079
alumno <alumno>
2018-09-04 21:21:05 -0300

    Clase 5
    Ejercicio hecho en clase. Sobrecargas.

---
+++ Clase5/Repaso/Repaso/Estante.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso
+{
+  public class Estante
+  {
+    private Producto[] productos;
+    private int ubicacionEstante;
+
+    private Estante(int capacidad)
+    {
+      productos = new Producto[capacidad];
+    }
+
+    public Estante (int capacidad, int ubicacion):this(capacidad)
+    {
+      this.ubicacionEstante = ubicacion;
+    }
+
+    public Producto[] GetProductos()
+    {
+      return this.productos;
+    }
+
+    // Es estàtico ya que le tengo que pasar un objeto de la clase. Si no recibiera parámetros, podría no ser estático.
+    public static string MostrarEstante(Estante e)
+    {
+      StringBuilder sb = new StringBuilder();
+      sb.AppendLine(string.Format("Estante nro {0}",e.ubicacionEstante.ToString()));
+      
+      for (int i = 0; i < e.GetProductos().Length; i++)
+      {        
+        sb.AppendLine(Producto.MostrarProducto(e.GetProductos()[i]));
+        sb.AppendLine();
+      }
+
+      return sb.ToString();
+    }
+
+    public static bool operator ==(Estante e, Producto p)
+    {
+      bool yaExiste = false;
+
+      for (int i = 0; i<e.GetProductos().Length; i++)
+      {
+        if(e.GetProductos()[i] == p)
+        {
+          yaExiste = true;
+          break;
+        }
+      }
+
+      return yaExiste;
+    }
+
+    // True si p no está en el estante. False si está p en el estante.
+    public static bool operator !=(Estante e, Producto p)
+    {
+      bool noExiste = true;
+
+      for (int i = 0; i < e.GetProductos().Length; i++)
+      {
+        if (e.GetProductos()[i] == p)
+        {
+          noExiste = false;
+          break;
+        }
+      }
+
+      return noExiste;
+    }
+
+    /// <summary>
+    /// Adición
+    /// </summary>
+    /// <param name="e"></param>
+    /// <param name="p"></param>
+    /// <returns>retornará true y agregará el producto si el estante posee capacidad de almacenar al menos un producto más y dicho producto no se encuentra en él; </returns>
+    public static bool operator +(Estante e, Producto p)
+    {
+      bool agregado = false;      
+      
+      if (e != p)
+      {
+        for (int i = 0; i < e.GetProductos().Length; i++)
+        {
+          if (e.GetProductos()[i] is null)
+          {
+            e.GetProductos()[i] = p;
+            agregado = true;
+            break;
+          }
+        }
+      }
+
+      return agregado;      
+    }
+
+    /// <summary>
+    /// Sustracción
+    /// </summary>
+    /// <param name="e"></param>
+    /// <param name="p"></param>
+    /// <returns> retornará un estante sin el producto, siempre y cuando el producto se encuentre en el listado</returns>
+    public static Estante operator -(Estante e, Producto p)
+    {
+      Estante estante = new Estante(e.GetProductos().Length);
+      estante = e;
+
+      if (estante == p)
+      {
+        for(int i= 0; i<estante.GetProductos().Length; i++)
+        {
+          if (p == estante.GetProductos()[i])
+          {
+            estante.GetProductos()[i] = null;
+            break;
+          }
+        }
+      }
+
+      return estante;
+    }
+  }
+}
---
+++ Clase5/Repaso/Repaso/Producto.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso
+{
+  public class Producto
+  {
+    private string codigoDeBarra;
+    private string marca;
+    private float precio;
+
+    public Producto(string marca, string codigo, float precio)
+    {
+      this.marca = marca;
+      this.codigoDeBarra = codigo;
+      this.precio = precio;
+    }
+
+    public string GetMarca()
+    {
+      return this.marca;
+    }
+
+    public float GetPrecio()
+    {
+      return this.precio;
+    }
+
+    // Es estàtico ya que le tengo que pasar un objeto de la clase. Si no recibiera parámetros, podría no ser estático.
+    public static string MostrarProducto (Producto p)
+    {
+      string retorno;
+      if (p is null) retorno = string.Empty; 
+      else
+      {
+        retorno = string.Format("Marca: {0}\nCódigo de barra: {1}\nPrecio: {2}",
+        p.GetMarca(), (string)p, p.GetPrecio().ToString());
+      }
+      return retorno;
+    }
+
+    public static explicit operator string (Producto p)
+    {
+      string retorno;
+      if (p is null) retorno = string.Empty;
+      else
+      {
+        retorno = p.codigoDeBarra;
+      }
+      return retorno; 
+    }
+
+    public static bool operator == (Producto p, string marca)
+    {
+      bool esIgual;
+
+      if (p is null || string.IsNullOrEmpty(marca)) esIgual = false;
+      else esIgual = (p.GetMarca() == marca);
+
+      return esIgual; 
+    }
+
+    public static bool operator !=(Producto p, string marca)
+    {
+      return (p.GetMarca() != marca);
+    }
+
+    public static bool operator ==(Producto p, Producto p2)
+    {
+      bool iguales;
+
+      if (p is null || p2 is null) iguales = false;
+      else
+      {
+        iguales = (p.GetMarca() == p2.GetMarca() && (string)p == (string)p2);
+      }
+
+      return iguales;
+    }
+
+    public static bool operator !=(Producto p, Producto p2)
+    {
+      return (p.GetMarca() != p2.GetMarca() || (string)p != (string)p2);
+    }
+
+  }
+}
