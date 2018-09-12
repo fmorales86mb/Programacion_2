074111e9c866c614a50479d69030c489e5192f96
fmorales86mb <fmorales86mb@gmail.com>
2018-09-12 15:55:41 -0300

    Ejercicio 13
    Se realiza lo fundamental de los metodos bin a dec y dec a bin. Falta
    ver nros con decimales.

--- Guía de Ejercicios/02_MetodosEstaticos/BibliotecaMetodosEstaticos/Conversor.cs
+++ Guía de Ejercicios/02_MetodosEstaticos/BibliotecaMetodosEstaticos/Conversor.cs
@@ -8,6 +8,11 @@ namespace BibliotecaMetodosEstaticos
 {
     public class Conversor
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="valor"></param>
+        /// <returns></returns>
         public static string DecimalBinario(double valor)
         {
             string nroBinario = string.Empty;
@@ -16,16 +21,44 @@ namespace BibliotecaMetodosEstaticos
             {
                 if (valor % 2 == 0) nroBinario += '0';
                 else nroBinario += '1';
-                    valor = valor / 2;
+
+                valor = (int)(valor / 2); // tomo la parte entera
                 
             } while (valor >= 2);
 
             return nroBinario;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="valor"></param>
+        /// <returns></returns>
         public static double BinarioDecimal(string valor)
         {
-            return 2;
+            double acumulador = 0;            
+            int nro;
+            double factorProducto;
+            int j = valor.Length -1;
+
+            for(int i = 0; i<valor.Length; i++, j--)
+            {
+                // voy de derecha a izquierda. El factor arranca en 1 con i = 0
+                factorProducto = Math.Pow(2, i); 
+
+                // La toma de dígitos es de derecha a izquierda. con j igual al último índice del string.
+                if (int.TryParse(valor[j].ToString(), out nro))
+                {
+                    acumulador += nro * factorProducto;
+                }
+                else
+                {
+                    acumulador = 0;
+                    break;
+                }
+            }
+
+            return acumulador;
         }
     }
 }
--- Guía de Ejercicios/02_MetodosEstaticos/Ejercicio_13/Program.cs
+++ Guía de Ejercicios/02_MetodosEstaticos/Ejercicio_13/Program.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BibliotecaMetodosEstaticos;
 
 namespace Ejercicio_13
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
+        static void Main(string[] args)
+        {
+            string nroBin = "10100001";
+            double nroDec = 21;
+
+            Console.WriteLine(string.Format("{0} (2) -> {1} (10)", 
+                nroBin, (Conversor.BinarioDecimal(nroBin)).ToString()));
+
+            Console.WriteLine(string.Format("{0} (10) -> {1} (2)",
+                nroDec.ToString(), Conversor.DecimalBinario(nroDec)));
+
+            Console.ReadKey();
+        }
     }
-  }
 }
