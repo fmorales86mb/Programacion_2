215b1b192254b79741457bd2c58213a20a773fb7
fmorales86mb <fmorales86mb@gmail.com>
2018-09-18 11:56:32 -0300

    Ejercicio 26
    Resuelto.

---
+++ Guía de Ejercicios/07_ArraysYColecciones/BiblioColecciones/Ordenado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblioColecciones
+{
+    public class Ordenado
+    {
+        /// <summary>
+        /// Compara dos valores.
+        /// </summary>
+        /// <param name="a"> primer valor</param>
+        /// <param name="b"> segundo valor</param>        
+        /// <returns>a menor que b, -1; a mayor q b, 1; a igual a b, 0 </returns>
+        public static int Comparador (double  a, double b)
+        {
+            int retorno = 0;
+            if (a > b) retorno = 1;
+            else if (a < b) retorno = -1;
+            return retorno;
+        }
+    }
+}
---
+++ Guía de Ejercicios/07_ArraysYColecciones/Ejercicio_26/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BiblioColecciones;
+
+namespace Ejercicio_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 26";
+
+            int aux;
+            int[] vector = new int[20];
+            Random rand = new Random();
+
+            // Creo el vector de nros random
+            for (int i = 0; i < vector.Length; i++)
+            {
+                // Creo un random que es positivo y le asigno signo.
+                aux = rand.Next();
+                aux = aux * (rand.NextDouble() > 0.5 ? 1 : -1);                
+                vector[i] = aux;
+            }
+
+            // Muestro el vector 
+            Console.WriteLine("Muestro el vector:");
+            Console.WriteLine();
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.WriteLine(vector[i]);
+            }
+            Console.WriteLine();
+
+            // Muestro positivos en forma decreciente.            
+            Console.WriteLine("Muestro positivos en forma decreciente:");
+            Console.WriteLine();
+            for (int i = 0; i < vector.Length -1; i++)
+            {
+                for (int j = i+1; j < vector.Length; j++)
+                {
+                    if (Ordenado.Comparador(vector[i], vector[j]) < 0)
+                    {
+                        aux = vector[i];
+                        vector[i] = vector[j];
+                        vector[j] = aux;
+                    }
+                }                
+            }
+            for (int i = 0; i < vector.Length; i++)
+            {
+                if (vector[i]>0) Console.WriteLine(vector[i]);
+            }
+            Console.WriteLine();
+
+            // Muestro negativos en forma creciente.
+            Console.WriteLine("Muestro negativos en forma creciente:");
+            Console.WriteLine();
+            for (int i = vector.Length -1; i >=0 ; i--)
+            {
+                if (vector[i]<0) Console.WriteLine(vector[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
