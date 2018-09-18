b61c3054230afe89e8e8192bc38f4bc9f2fc197b
fmorales86mb <fmorales86mb@gmail.com>
2018-09-18 17:39:35 -0300

    Ejercicio 29
    Se crea parte de las estructuras de las clases.

--- Guía de Ejercicios/07_ArraysYColecciones/BiblioColecciones/Jugador.cs
+++ Guía de Ejercicios/07_ArraysYColecciones/BiblioColecciones/Jugador.cs
@@ -8,5 +8,47 @@ namespace BiblioColecciones
 {
     class Jugador
     {
+        private long dni;
+        private string nombre;
+        private int partidosJugados;
+        private float promedioGoles;
+        private int totalGoles;
+
+        public float GetPromedioGoles()
+        {
+            this.promedioGoles = (float)totalGoles / partidosJugados;
+            return promedioGoles;
+        }
+
+        private Jugador()
+        {
+            this.partidosJugados = 0;
+            this.totalGoles = 0;
+        }
+
+        public Jugador(long dni, string nombre) : this()
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+            this.promedioGoles = 0;
+        }
+
+        public Jugador(long dni, string nombre, int partidosJugados, int totalGoles):this(dni, nombre)
+        {
+            this.partidosJugados = partidosJugados;
+            this.totalGoles = totalGoles;
+        }
+        
+        public string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Nombre: " + this.nombre);
+            sb.AppendLine("DNI: " + this.dni.ToString());
+            sb.AppendLine("Partidos Jugados: " + this.partidosJugados.ToString());
+            sb.AppendLine("Goles totales: " + this.totalGoles.ToString());
+            sb.AppendLine("Promedio de gol: " + this.GetPromedioGoles().ToString());
+            sb.AppendLine();
+            return sb.ToString();
+        }
     }
 }
