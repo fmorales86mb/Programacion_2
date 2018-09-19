50045620111e0e78500b3da1560cfa98a115bbec
alumno <alumno>
2018-09-18 22:09:58 -0300

    Ejercicio 33
    En proceso.

---
+++ Guía de Ejercicios/08_Encapsulamiento/Entidades/Libro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Libro
+    {
+        private List<string> paginas;
+
+        public string this[int i]
+        {
+            get
+            {
+                string pagina = string.Empty;
+                if (i < this.paginas.Count())                
+                    pagina = this.paginas[i];
+
+                return pagina;                
+            }
+            set { }
+        }
+            
+
+    }
+}
