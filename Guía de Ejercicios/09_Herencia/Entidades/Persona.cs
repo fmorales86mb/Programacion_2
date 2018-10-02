067e05ed4c79b949e3f502fdb5aa80fe7d8cb797
fmorales86mb <fmorales86mb@gmail.com>
2018-10-02 11:58:41 -0300

    Ejercicio 35
    Resuleto.

---
+++ Guía de Ejercicios/09_Herencia/Entidades/Persona.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Persona
+    {
+        private long dni;
+        private string nombre;
+
+        public long Dni
+        {
+            get 
+            {
+                return dni;
+            }
+            set
+            {
+                this.dni = value;
+            }
+        }
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+            set
+            {
+                this.nombre = value;
+            }
+        }
+
+        public Persona(string nombre) 
+        {
+            this.Nombre = nombre;
+        }
+        public Persona(long dni, string nombre)
+            : this(nombre)
+        {
+            this.Dni = dni;
+        }
+
+        public virtual string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("Nombre: {0}", this.Nombre);
+            sb.AppendFormat("Dni: {0}", this.Dni);
+
+            return sb.ToString();
+        }
+    }
+}
