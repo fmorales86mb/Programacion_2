f9c94308b2be3dff2884785cbe86af1e687b2662
fmorales86mb <fmorales86mb@gmail.com>
2018-10-02 17:10:27 -0300

    Ejercicio 36
    Clase Competencia incompleta. Las otras clases, implementadas.

---
+++ Guía de Ejercicios/09_Herencia/Entidades/MotoCross.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class MotoCross : VehiculoDeCarrera
+    {
+        private short cilindrada;
+
+        public short Cilindrada
+        {
+            get
+            {
+                return this.cilindrada;
+            }
+            set
+            {
+                this.cilindrada = value;
+            }
+        }
+
+        public MotoCross(short numero, string escuderia)
+            : base(numero, escuderia)
+        { }
+        public MotoCross(short numero, string escuderia, short cilindrada)
+            : this(numero, escuderia)
+        {
+            this.Cilindrada = cilindrada;
+        }
+
+        public static bool operator ==(MotoCross moto1, MotoCross moto2)
+        {
+            return (moto1 == moto2 && moto1.Cilindrada == moto2.Cilindrada);
+        }
+        public static bool operator !=(MotoCross moto1, MotoCross moto2)
+        {
+            return !(moto1 == moto2);
+        }
+    }
+}
