b48bf5b5b779e524372acac19328456f15d8ae42
fmorales86mb <fmorales86mb@gmail.com>
2018-10-03 16:25:58 -0300

    Ejercicios 38 y 39
    Resueltos.

---
+++ Guía de Ejercicios/10_Polimorfismo/Ejercicio_38/Sobreescrito.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_38
+{
+    public class Sobreescrito
+    {
+        public override string ToString()
+        {
+            return "Este es mi método ToString sobreeescito";
+        }
+        public override bool Equals(object obj)
+        {
+            return (obj is Sobreescrito);
+        }
+        public override int GetHashCode()
+        {
+            return 1142510187;
+        }
+    }
+}
---
+++ Guía de Ejercicios/10_Polimorfismo/Ejercicio_39/Sobreescrito.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_39
+{
+    public abstract class Sobreescrito
+    {
+        protected string miAtributo;
+
+        public abstract string MiPropiedad
+        {
+            get;
+        }
+
+        public Sobreescrito()
+        {
+            this.miAtributo = "Probar Abstractos";
+        }
+
+        public abstract string MiMetodo();
+
+        public override string ToString()
+        {
+            return "Este es mi método ToString sobreeescito";
+        }
+        public override bool Equals(object obj)
+        {
+            return (obj is Sobreescrito);
+        }
+        public override int GetHashCode()
+        {
+            return 1142510187;
+        }
+    }
+}
