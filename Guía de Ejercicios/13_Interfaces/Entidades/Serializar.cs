008ff7f66f8597ee0f29e4f6d90301553c65839a
alumno <alumno>
2018-10-23 21:49:04 -0300

    Ejercicio 50
    Resuelto - Interfaces

---
+++ Guía de Ejercicios/13_Interfaces/Entidades/Serializar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Serializar<S, V> : IGuardar<S, V>
+    {
+        public bool Guardar(S obj)
+        {
+            return true;
+        }
+
+        public V Leer()
+        {
+            return (V)Convert.ChangeType("Objeto Leido", typeof(V));
+        }
+    }
+}
